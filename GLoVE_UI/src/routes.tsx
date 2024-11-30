69daa992d6fcbf7f35feac5a0468518c62fe8dcd
PanosGidarakos <panosgidarakos@gmail.com>
2024-11-30 18:42:17 +0000

    strip-down ui

--- GLoVE_UI/src/routes.tsx
+++ GLoVE_UI/src/routes.tsx
@@ -1,20 +1,10 @@
 import { createBrowserRouter } from "react-router-dom"
-import ProgressPage from "./app/ProgressPage/progress-page";
 import ErrorPage from "./error-page";
 import NotFound from "./not-found";
 import GlanceComponent from "./app/Tasks/GlanceTask/GlanceComponent";
 
 const routes = createBrowserRouter([
-  {
-    path: "/:experimentId",
-    element: <ProgressPage />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-    errorElement: <NotFound />
-  },
+ 
   {
     path: "/glance",
     element: <GlanceComponent />,
--- GLoVE_UI/src/store/store.ts
+++ GLoVE_UI/src/store/store.ts
@@ -1,16 +1,12 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 import { configureStore } from "@reduxjs/toolkit"
-import { dataExplorationSlice } from "./slices/dataExplorationSlice"
-import { workflowTabsSlice } from "./slices/workflowTabsSlice"
-import { progressPageSlice } from "./slices/progressPageSlice"
+
 import glanceReducer from "./slices/glanceSlice"; // Import glance slice
 
 
 export const store = configureStore({
   reducer: {
-    dataExploration: dataExplorationSlice.reducer,
-    workflowTabs: workflowTabsSlice.reducer,
-    progressPage: progressPageSlice.reducer,   
+ 
     glance: glanceReducer, // Add glance slice to the reducers
 
   },
